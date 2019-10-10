07f13bbe56e2ccbdcbca591376b2b901c041dd59
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-10 18:25:29 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,38 +11,6 @@ class Song
     @@count
   end
 
-  # def artist=(artist)
-  #   if @artist != nil
-  #     @@artist_count[artist] -= 1
-  #     if @@artist_count[artist] <= 0
-  #       @@artists = @@artists.reject{|included_artist| included_artist == artist}
-  #     end
-  #   end
-  #   @artist = artist
-  #   @@artists << artist
-  #   if !@@artist_count.has_key?(artist)
-  #     @@artist_count[artist] = 1
-  #   else
-  #     @@artist_count[artist] += 1
-  #   end
-  # end
-  #
-  # def genre=(genre)
-  #   if @genre != nil
-  #     @@genre_count[genre] -= 1
-  #     if @@genre_count[genre] <= 0
-  #       @@genres = @@genres.reject{|included_genre| included_genre == genre}
-  #     end
-  #   end
-  #   @genre = genre
-  #   @@genres << genre
-  #   if !@@genre_count.has_key?(genre)
-  #     @@genre_count[genre] = 1
-  #   else
-  #     @@genre_count[genre] += 1
-  #   end
-  # end
-
   def self.genres
     @@genres.uniq
   end
