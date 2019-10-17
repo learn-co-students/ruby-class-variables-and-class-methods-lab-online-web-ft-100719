2a5def3f0fe3def7ead814275a72a3740a86f9d4
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-17 03:08:16 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,52 @@
+require 'pry'
+
+class Song
+
+attr_accessor :name, :artist, :genre  
+@@count=0
+@@artists=[]
+@@genres=[]
+@@genre_count={}
+@@artist_count={}
+
+
+  def initialize(name,artist,genre)
+    @name=name
+    @artist=artist
+    @genre=genre
+    @@count+=1
+    @@artists<< artist
+    @@genres<< genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artist_count
+    counted_artists={}
+    @@artists.each do |artist|
+      counted_artists[artist]
+      if counted_artists[artist]
+        counted_artists[artist]+=1
+      else
+        counted_artists[artist]=1
+      end
+      # @@artist_hash[artist]=artist.length
+      counted_artists
+    end
+  end
+  
+  def self.genre_count
+    @genre<<@@genres.length
+  end
+  
+end
