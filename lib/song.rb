756854d4211293bf3ccd10db1960360b78f73d7a
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-11-12 17:48:43 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count=0
+  @@genres=[]
+  @@artists=[]
+  def initialize(name,artist,genre)
+    @@count+=1
+    @name=name
+    @artist=artist
+    @genre=genre
+  end
+  def self.count
+    @@count
+  end
+  def self.genres
+    @@genres<<@genres.uniq
+  end
+  def self.artists
+    @@artists<<@artist.uniq
+  end
+end
