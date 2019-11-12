c6580bee7eff1a678445bd249ae394666efd2e9f
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-11-12 18:40:04 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -8,14 +8,37 @@ class Song
     @name=name
     @artist=artist
     @genre=genre
+    @@artists<<@artist
+    @@genres<<@genre
   end
   def self.count
     @@count
   end
   def self.genres
-    @@genres<<@genres.uniq
+    @@genres.uniq
   end
   def self.artists
-    @@artists<<@artist.uniq
+    @@artists.uniq
+  end
+  def self.genre_count
+    genre_count={}
+    @@genres.each do |x|
+      if genre_count[x]
+        genre_count[x]+=1
+      else genre_count[x]=1
+      end
+    end
+    genre_count
+  end
+  def self.artist_count
+    artist_count={}
+    @@artists.each do |art|
+      if artist_count[art]
+        artist_count[art]+=1
+      else
+        artist_count[art]=1
+      end
+    end
+    artist_count
   end
 end
